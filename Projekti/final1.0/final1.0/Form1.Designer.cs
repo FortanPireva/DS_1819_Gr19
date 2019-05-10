468ae734e7d28ddfbbbf31e3eeb5e087dca9191f
LabinotDragusha <44063739+LabinotDragusha@users.noreply.github.com>
2019-05-10 02:09:30 +0200

    projekti

---
+++ Projekti/final1.0/final1.0/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace final1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.btnEnkripto = new System.Windows.Forms.Button();
+            this.txtCiphertext = new System.Windows.Forms.TextBox();
+            this.btnDekripto = new System.Windows.Forms.Button();
+            this.txtTextiDekriptuar = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textQelsi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtPlaintext.Location = new System.Drawing.Point(80, 42);
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(644, 26);
+            this.txtPlaintext.TabIndex = 0;
+            this.txtPlaintext.Text = "Message";
+            this.txtPlaintext.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnEnkripto
+            // 
+            this.btnEnkripto.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEnkripto.BackgroundImage")));
+            this.btnEnkripto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnEnkripto.Location = new System.Drawing.Point(659, 108);
+            this.btnEnkripto.Name = "btnEnkripto";
+            this.btnEnkripto.Size = new System.Drawing.Size(65, 62);
+            this.btnEnkripto.TabIndex = 1;
+            this.btnEnkripto.UseVisualStyleBackColor = true;
+            this.btnEnkripto.Click += new System.EventHandler(this.btnEnkripto_Click);
+            // 
+            // txtCiphertext
+            // 
+            this.txtCiphertext.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtCiphertext.Location = new System.Drawing.Point(79, 211);
+            this.txtCiphertext.Name = "txtCiphertext";
+            this.txtCiphertext.Size = new System.Drawing.Size(645, 26);
+            this.txtCiphertext.TabIndex = 2;
+            // 
+            // btnDekripto
+            // 
+            this.btnDekripto.BackColor = System.Drawing.Color.DarkGray;
+            this.btnDekripto.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDekripto.BackgroundImage")));
+            this.btnDekripto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDekripto.Location = new System.Drawing.Point(658, 277);
+            this.btnDekripto.Name = "btnDekripto";
+            this.btnDekripto.Size = new System.Drawing.Size(66, 62);
+            this.btnDekripto.TabIndex = 3;
+            this.btnDekripto.UseVisualStyleBackColor = false;
+            this.btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
+            // 
+            // txtTextiDekriptuar
+            // 
+            this.txtTextiDekriptuar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtTextiDekriptuar.Location = new System.Drawing.Point(79, 391);
+            this.txtTextiDekriptuar.Name = "txtTextiDekriptuar";
+            this.txtTextiDekriptuar.Size = new System.Drawing.Size(645, 26);
+            this.txtTextiDekriptuar.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(79, 125);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Shiko Celesin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textQelsi
+            // 
+            this.textQelsi.Location = new System.Drawing.Point(274, 125);
+            this.textQelsi.Name = "textQelsi";
+            this.textQelsi.Size = new System.Drawing.Size(362, 26);
+            this.textQelsi.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textQelsi);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtTextiDekriptuar);
+            this.Controls.Add(this.btnDekripto);
+            this.Controls.Add(this.txtCiphertext);
+            this.Controls.Add(this.btnEnkripto);
+            this.Controls.Add(this.txtPlaintext);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPlaintext;
+        private System.Windows.Forms.Button btnEnkripto;
+        private System.Windows.Forms.TextBox txtCiphertext;
+        private System.Windows.Forms.Button btnDekripto;
+        private System.Windows.Forms.TextBox txtTextiDekriptuar;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textQelsi;
+    }
+}
+
---
+++ Projekti/final1.0/final1.0/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace final1._0
+{
+    public partial class Form1 : Form
+    {
+        private DR subsituteObject = new DR();
+        private int key;
+        byte[] encQuote;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnkripto_Click(object sender, EventArgs e)
+        {
+            subsituteObject.GenerateKey();
+            key = DR.key;
+            string plaintexti = txtPlaintext.Text;
+            encQuote = EncryptString(subsituteObject, plaintexti);
+
+            txtCiphertext.Text = Convert.ToBase64String(encQuote);
+            if (checkBox1.Checked)
+            {
+                textQelsi.Text = " " + key;
+            }
+        }
+
+        private void btnDekripto_Click(object sender, EventArgs e)
+        {
+            string plaintexti = DecryptBytes(subsituteObject, encQuote);
+
+            txtTextiDekriptuar.Text = plaintexti;
+        }
+        public static byte[] EncryptString(SymmetricAlgorithm symAlg, string inString)
+        {
+            byte[] inBlock = Encoding.ASCII.GetBytes(inString);
+            ICryptoTransform xfrm = symAlg.CreateEncryptor();
+            byte[] outBlock = xfrm.TransformFinalBlock(inBlock, 0, inBlock.Length);
+
+            return outBlock;
+        }
+        public static string DecryptBytes(SymmetricAlgorithm symAlg, byte[] inBytes)
+        {
+            ICryptoTransform xfrm = symAlg.CreateDecryptor();
+            byte[] outBlock = xfrm.TransformFinalBlock(inBytes, 0, inBytes.Length);
+
+            return Encoding.ASCII.GetString(outBlock);
+        }
+        class DR : SymmetricAlgorithm
+        {
+            public static int key;//symmetric key used for encryption and decryption
+            public override int BlockSize { set => base.BlockSize = 8; }//mutator method to set block size of operation to 8 bits.
+            public override byte[] Key { set => base.Key = BitConverter.GetBytes(key); }
+            public override int KeySize { get => base.KeySize; set => base.KeySize = 64; }
+
+
+            public override ICryptoTransform CreateEncryptor()
+            {
+
+                ETransform encrypt = new ETransform(key);
+
+                return encrypt;
+            }
+            public override ICryptoTransform CreateDecryptor()
+            {
+                DTransform decrypt = new DTransform(key);
+                return decrypt;
+            }
+
+            public override ICryptoTransform CreateEncryptor(byte[] rgbKey, byte[] rgbIV)
+            {
+                ETransform encrypt = new ETransform(key);
+                return encrypt;
+            }
+
+            public override ICryptoTransform CreateDecryptor(byte[] rgbKey, byte[] rgbIV)
+            {
+                DTransform decrypt = new DTransform(key);
+                return decrypt;
+            }
+
+            public override void GenerateKey()
+            {
+                DR.key = RandomKey();
+            }
+
+            public override void GenerateIV()
+            {
+
+            }
+            public int RandomKey()
+            {
+                Random rnd = new Random();
+                int[] array = new int[8];
+                array[0] = rnd.Next(1, 9);
+                int[] arraycopy = new int[8];
+                arraycopy[0] = array[0];
+
+                string numri = "" + array[0];
+                for (int i = 1; i < 8; i++)
+                {
+
+                    while (isInArray(arraycopy, array[i]))
+                    {
+                        array[i] = rnd.Next(1, 9);
+
+                    }
+                    arraycopy[i] = array[i];
+                    numri += array[i];
+
+
+                }
+                key = Int32.Parse(numri);
+                return key;
+            }
+
+            private bool isInArray(int[] arr, int nr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (nr == arr[i])
+                        return true;
+
+                }
+                return false;
+            }
+
+        }
+        class Program
+        {
+
+
+            public static byte[] EncryptString(SymmetricAlgorithm symAlg, string inString)
+            {
+                byte[] inBlock = Encoding.ASCII.GetBytes(inString);
+                ICryptoTransform xfrm = symAlg.CreateEncryptor();
+                byte[] outBlock = xfrm.TransformFinalBlock(inBlock, 0, inBlock.Length);
+
+                return outBlock;
+            }
+
+            public static string DecryptBytes(SymmetricAlgorithm symAlg, byte[] inBytes)
+            {
+                ICryptoTransform xfrm = symAlg.CreateDecryptor();
+                byte[] outBlock = xfrm.TransformFinalBlock(inBytes, 0, inBytes.Length);
+
+                return Encoding.ASCII.GetString(outBlock);
+            }
+            private static int[] Bits(byte c)//funksioni qe nxjerr si array te integerave prej 1 ose 0
+            {
+                int[] bit = new int[8];
+                for (int i = 0; i < 8; i++)
+                {
+                    bit[7 - i] = ((c >> i) & 0x01);
+                }
+                return bit;
+            }
+
+
+        }
+        class ETransform : ICryptoTransform
+        {
+            public int InputBlockSize => 1;
+
+            public int OutputBlockSize => 1;
+
+            public bool CanTransformMultipleBlocks => false;
+
+            public bool CanReuseTransform => false;
+
+            private int key;
+            public ETransform(int key)
+            {
+                this.key = key;
+            }
+
+            public void Dispose()
+            {
+                // This object will be cleaned up by the Dispose method.
+                // Therefore, you should call GC.SupressFinalize to
+                // take this object off the finalization queue
+                // and prevent finalization code for this object
+                // from executing a second time.
+                GC.SuppressFinalize(this);
+            }
+
+            public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+            {
+                int[] shifrat = digitArr(key);
+
+                for (int i = inputOffset; i < inputCount; i++)
+                {
+                    int[] bitat = Bits(inputBuffer[i]);
+                    int[] newbits = new int[8];
+                    for (int j = 0; j < newbits.Length; j++)
+                    {
+                        newbits[j] = bitat[shifrat[j] - 1];
+
+                    }
+                    outputBuffer[outputOffset + i] = toByte(newbits);
+
+
+                }
+                return outputBuffer.Length;
+            }
+
+            public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+            {
+                int[] shifrat = digitArr(key);
+                byte[] outputBuffer = new byte[inputCount];
+                for (int i = inputOffset; i < inputCount; i++)
+                {
+                    int[] bitat = Bits(inputBuffer[i]);
+                    int[] newbits = new int[8];
+
+
+                    for (int j = 0; j < newbits.Length; j++)
+                    {
+
+                        newbits[j] = bitat[shifrat[j] - 1];
+
+                    }
+
+                    outputBuffer[i] = toByte(newbits);
+
+
+                }
+                return outputBuffer;
+
+            }
+            private int[] Bits(byte c)//funksioni qe nxjerr si array te integerave prej 1 ose 0
+            {
+                int[] bit = new int[8];
+                for (int i = 0; i < 8; i++)
+                {
+                    bit[7 - i] = ((c >> i) & 0x01);
+                }
+                return bit;
+            }
+            private int[] digitArr(int n)
+            {
+                if (n == 0) return new int[1] { 0 };
+
+                var digits = new List<int>();
+
+                for (; n != 0; n /= 10)
+                    digits.Add(n % 10);
+
+                var arr = digits.ToArray();
+                Array.Reverse(arr);
+                return arr;
+            }
+            private byte toByte(int[] bitDigits)
+            {
+                int numri = 0;
+                int k = 1;
+
+                for (int i = 0; i < bitDigits.Length; i++)
+                {
+                    if (bitDigits[i] == 1)
+                        numri += (int)Math.Pow(2, bitDigits.Length - k);
+                    k++;
+
+
+                }
+                return (byte)numri;
+            }
+
+
+        }
+        class DTransform : ICryptoTransform
+        {
+            public int InputBlockSize => 1;
+
+            public int OutputBlockSize => 1;
+
+            public bool CanTransformMultipleBlocks => false;
+
+            public bool CanReuseTransform => false;
+            private int key;
+            public DTransform(int key)
+            {
+                this.key = key;
+            }
+            public void Dispose()
+            {
+                // This object will be cleaned up by the Dispose method.
+                // Therefore, you should call GC.SupressFinalize to
+                // take this object off the finalization queue
+                // and prevent finalization code for this object
+                // from executing a second time.
+                GC.SuppressFinalize(this);
+            }
+
+            public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+            {
+                int[] shifrat = digitArr(key);
+
+                for (int i = inputOffset; i < inputCount; i++)
+                {
+                    int[] bitat = Bits(inputBuffer[i]);
+                    int[] newbits = new int[8];
+                    for (int j = 0; j < newbits.Length; j++)
+                    {
+                        newbits[shifrat[j] - 1] = bitat[j];
+                    }
+                    outputBuffer[outputOffset + i] = toByte(newbits);
+
+
+                }
+                return outputBuffer.Length;
+            }
+
+            public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+            {
+                int[] shifrat = digitArr(key);
+                byte[] outputBuffer = new byte[inputCount];
+                for (int i = inputOffset; i < inputCount; i++)
+                {
+                    int[] bitat = Bits(inputBuffer[i]);
+                    int[] newbits = new int[8];
+                    for (int j = 0; j < newbits.Length; j++)
+                    {
+                        newbits[shifrat[j] - 1] = bitat[j];
+                    }
+                    outputBuffer[i] = toByte(newbits);
+
+
+                }
+                return outputBuffer;
+            }
+            private int[] Bits(byte c)//funksioni qe nxjerr si array te integerave prej 1 ose 0
+            {
+                int[] bit = new int[8];
+                for (int i = 0; i < 8; i++)
+                {
+                    bit[7 - i] = ((c >> i) & 0x01);
+                }
+                return bit;
+            }
+            private int[] digitArr(int n)
+            {
+                if (n == 0) return new int[1] { 0 };
+
+                var digits = new List<int>();
+
+                for (; n != 0; n /= 10)
+                    digits.Add(n % 10);
+
+                var arr = digits.ToArray();
+                Array.Reverse(arr);
+                return arr;
+            }
+            private byte toByte(int[] bitDigits)
+            {
+                int numri = 0;
+                int k = 1;
+
+                for (int i = 0; i < bitDigits.Length; i++)
+                {
+                    if (bitDigits[i] == 1)
+                        numri += (int)Math.Pow(2, bitDigits.Length - k);
+                    k++;
+
+
+                }
+                return (byte)numri;
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textQelsi.Text = " " + key;
+            }
+        }
+    }
+}
